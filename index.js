8e4de54a78f8f85a225f258d6ca81b9b1b00dddb
Ari Khaytser <ari.khaytser@gmail.com>
2024-12-01 23:57:42 -0500

    Get needed events from HebCal

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const app = express()
 const port = 3000
 const {HebrewCalendar, HDate, Location, Event} = require('@hebcal/core')
 
-var hebDate = new HDate(new Date())
+var hebDates = [...Array(3).keys()].map(i => new HDate(new Date()).add(i, 'd'))
+console.log(hebDates)
+
+const options  = {
+  year: hebDates[0].getFullYear(),
+  isHebrewYear: true,
+  candlelighting: true,
+  location: Location.lookup('San Francisco'),
+  sedrot: true,
+  omer: true,
+};
+
+const events = HebrewCalendar.calendar(options).filter(e => hebDates.some(d => d.isSameDate(e.date)))
+console.log(events)
 
 app.get('/', (req, res) => {
-  res.send(hebDate)
+  res.send(events)
 })
 
 app.listen(port, () => {
