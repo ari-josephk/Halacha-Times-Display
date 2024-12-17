7ccf9ba58bb7ac8cab52c11291ca0ce2e4cef5a7
Ari <ari.khaytser@gmail.com>
2024-12-16 23:30:58 -0500

    Improve date bar formatting

--- halacha-times-display/app/sidebar.js
+++ halacha-times-display/app/sidebar.js
@@ -1,16 +1,25 @@
 import styles from "./page.module.css";
+import { Fragment } from "react";
 
-const Sidebar = ({ elements }) => {
+const Sidebar = ({ elements: dateMap }) => {
   return (
     <div className={`${styles.sidebar} ${styles.main}`}>
       <ul>
-        {elements.map((e, index) => (
-          <li key={index} className={styles.capsule}>
-            <span>{e.getEmoji()}</span>
-            <span>{e.observedIn()}</span>
-            <span>{e.render()}</span>
-          </li>
-        ))}
+        {
+        [...dateMap.keys()].map((d => (
+          dateMap.get(d).length > 0 && (
+            <Fragment key={d}>
+              <li className={styles.dateNotifier}>{d.render()}</li>
+              {dateMap.get(d).map(e => (
+                <li className={styles.capsule}>
+                  <span>{e.getEmoji()}</span>
+                  <span>{e.observedIn()}</span>
+                  <span>{e.render()}</span>
+                </li>
+              ))}
+            </Fragment>
+          )
+        )))}
       </ul>
     </div>
   );
--- halacha-times-display/app/yomevents.js
+++ halacha-times-display/app/yomevents.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 async function getYomEvents() {
   const clientLocation = await getClientLocationFromIP();
-  var hebDates = [...Array(5).keys()].map(i => new HDate(new Date()).add(i - 1, 'd'))
+  var hebDates = [...Array(7).keys()].map(i => new HDate(new Date()).add(i - 1, 'd'))
 
   const options = {
     year: hebDates[0].getFullYear(),
@@ -12,15 +12,19 @@ async function getYomEvents() {
     location: clientLocation,
     sedrot: true,
     omer: true,
+    molad: true,
+    yomKippurKatan: true,
   };
 
-  return HebrewCalendar.calendar(options).filter(e => hebDates.some(d => d.isSameDate(e.date)))
+  const events = HebrewCalendar.calendar(options).filter(e => hebDates.some(d => d.isSameDate(e.date)));
+
+  return new Map(hebDates.map(d => [d, events.filter(e => d.isSameDate(e.date))]));
 }
 
 async function getClientLocationFromIP() {
   const response = await fetch('https://ipapi.co/json/');
   const data = await response.json();
-  return new Location(data.latitude, data.longitude, data.country === 'IL', data.timezone);
+  return new Location(data.latitude, data.longitude, data.country === 'IL', data.timezone, data.city);
 }
 
 export default getYomEvents;
