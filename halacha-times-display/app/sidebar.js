71a8f33689c5f425a351fa327cbc204289841d66
Ari <ari.khaytser@gmail.com>
2024-12-16 23:33:04 -0500

    Add message for no events

--- halacha-times-display/app/sidebar.js
+++ halacha-times-display/app/sidebar.js
@@ -2,25 +2,30 @@ import styles from "./page.module.css";
 import { Fragment } from "react";
 
 const Sidebar = ({ elements: dateMap }) => {
+  const hasEvents = [...dateMap.values()].some(events => events.length > 0);
+
   return (
     <div className={`${styles.sidebar} ${styles.main}`}>
-      <ul>
-        {
-        [...dateMap.keys()].map((d => (
-          dateMap.get(d).length > 0 && (
-            <Fragment key={d}>
-              <li className={styles.dateNotifier}>{d.render()}</li>
-              {dateMap.get(d).map(e => (
-                <li className={styles.capsule}>
-                  <span>{e.getEmoji()}</span>
-                  <span>{e.observedIn()}</span>
-                  <span>{e.render()}</span>
-                </li>
-              ))}
-            </Fragment>
-          )
-        )))}
-      </ul>
+      {hasEvents ? (
+        <ul>
+          {[...dateMap.keys()].map(d => (
+            dateMap.get(d).length > 0 && (
+              <Fragment key={d}>
+                <li className={styles.dateNotifier}>{d.render()}</li>
+                {dateMap.get(d).map(e => (
+                  <li className={styles.capsule} key={e.id}>
+                    <span>{e.getEmoji()}</span>
+                    <span>{e.observedIn()}</span>
+                    <span>{e.render()}</span>
+                  </li>
+                ))}
+              </Fragment>
+            )
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.dateNotifier}>No upcoming events</p>
+      )}
     </div>
   );
 };
