150d6e2f5e8b8d2f6b492f65d41c42026954576b
Ari <ari.khaytser@gmail.com>
2025-01-16 23:11:55 -0500

    Enable setting menu toggle using TV remote

--- flutter-app/halacha_times_display/lib/src/app.dart
+++ flutter-app/halacha_times_display/lib/src/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'settings/settings_controller.dart';
 
@@ -90,97 +92,140 @@ class MyApp extends StatelessWidget {
           'https://halacha-times-display.vercel.app/?city=$city&backgroundReloadMinutes=$backgroundTime'));
     }
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Halacha Times Display'),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text(
-                  'Settings',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-                ListTile(
-                title: Text('Select City'),
-                trailing: StatefulBuilder(
-                  builder: (BuildContext context, StateSetter setState) {
-                  return DropdownButton<String>(
-                    value: city,
-                    items: cities.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                    }).toList(),
-                    onChanged: (value) {
-                    setState(() {
-                      city = value!;
-                    });
-                    reloadWebview();
-                    },
-                  );
-                  },
-                ),
-                ),
-                ListTile(
-                title: Text('Change Background Time (in minutes)'),
-                trailing: SizedBox(
-                  width: 50,
-                  child: StatefulBuilder(
-                  builder: (BuildContext context, StateSetter setState) {
-                    return TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+    void toggleDrawer() async {
+      print("test");
+      if (scaffoldKey.currentState!.isDrawerOpen) {
+        scaffoldKey.currentState!.openEndDrawer();
+      } else {
+        scaffoldKey.currentState!.openDrawer();
+      }
+    }
+
+    void openDrawer() async {
+      if(!scaffoldKey.currentState!.isDrawerOpen) scaffoldKey.currentState!.openDrawer();
+    }
+
+    return KeyboardListener(
+      focusNode: FocusNode(),
+      onKeyEvent: (KeyEvent event) {
+        if (event.logicalKey == LogicalKeyboardKey.select) {
+          openDrawer();
+        }
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            WebViewWidget(
+                controller: webViewController
+                  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+                  ..setBackgroundColor(const Color(0x00000000))
+                  ..setNavigationDelegate(
+                    NavigationDelegate(
+                      onProgress: (int progress) {
+                        // Update loading bar.
+                      },
+                      onPageStarted: (String url) {},
+                      onPageFinished: (String url) {},
+                      onWebResourceError: (WebResourceError error) {},
+                      onNavigationRequest: (NavigationRequest request) {
+                        if (request.url.startsWith('https://')) {
+                          return NavigationDecision.navigate;
+                        }
+                        return NavigationDecision.prevent;
+                      },
                     ),
-                    controller: TextEditingController(text: backgroundTime.toString()),
-                    onChanged: (value) {
-                      setState(() {
-                      backgroundTime = int.parse(value);
-                      });
-                      reloadWebview();
-                    },
-                    );
-                  },
+                  )
+                  ..loadRequest(Uri.parse(
+                      'https://halacha-times-display.vercel.app/?city=Jerusalem'))),
+            GestureDetector(
+                onTap: () {
+                  toggleDrawer();
+                },
+                child: MaterialApp(
+                    home: Scaffold(
+                  key: scaffoldKey,
+                  backgroundColor: Colors.transparent,
+                  drawer: Drawer(
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      children: <Widget>[
+                        DrawerHeader(
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                          ),
+                          child: Text(
+                            'Settings',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          title: Text('Select City'),
+                          trailing: StatefulBuilder(
+                            builder:
+                                (BuildContext context, StateSetter setState) {
+                              return DropdownButton<String>(
+                                value: city,
+                                items: cities.map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    city = value!;
+                                  });
+                                  reloadWebview();
+                                },
+                              );
+                            },
+                          ),
+                        ),
+                        ListTile(
+                          title: Text('Change Background Time (in minutes)'),
+                          trailing: SizedBox(
+                            width: 50,
+                            child: StatefulBuilder(
+                              builder:
+                                  (BuildContext context, StateSetter setState) {
+                                return TextField(
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                  ),
+                                  controller: TextEditingController(
+                                      text: backgroundTime.toString()),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      backgroundTime = int.parse(value);
+                                    });
+                                    reloadWebview();
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          title: Text('Close Drawer'),
+                          trailing: IconButton(
+                            icon: Icon(Icons.close),
+                            onPressed: () {
+                              scaffoldKey.currentState!.openEndDrawer();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                ),
-            ],
-          ),
+                )))
+          ],
         ),
-        body: WebViewWidget(
-            controller: webViewController
-              ..setJavaScriptMode(JavaScriptMode.unrestricted)
-              ..setBackgroundColor(const Color(0x00000000))
-              ..setNavigationDelegate(
-                NavigationDelegate(
-                  onProgress: (int progress) {
-                    // Update loading bar.
-                  },
-                  onPageStarted: (String url) {},
-                  onPageFinished: (String url) {},
-                  onWebResourceError: (WebResourceError error) {},
-                  onNavigationRequest: (NavigationRequest request) {
-                    if (request.url.startsWith('https://')) {
-                      return NavigationDecision.navigate;
-                    }
-                    return NavigationDecision.prevent;
-                  },
-                ),
-              )
-              ..loadRequest(Uri.parse(
-                  'https://halacha-times-display.vercel.app/?city=Jerusalem'))),
       ),
     );
   }
